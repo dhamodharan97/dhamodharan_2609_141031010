9650a45a31c91c09a2cc22524a5cc55c7a34bd1e
dhamodharan97 <31979966+dhamodharan97@users.noreply.github.com>
2017-09-15 10:15:37 +0000

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Student s1=new Student(01,"Vijay",1997-02-14,80.00);
+		s1.getBirthDate();
+		s1.getAvgMark();
+		s1.getFullName();
+		s1.getId();
+		StudentGroup s2 = new StudentGroup(0);
+	    
+		
+
+	}
+
+}
